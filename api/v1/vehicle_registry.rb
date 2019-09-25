8f0c3ae8eb9f73f4bab32da3bf6ed896334e19d4
Julio Cesar Fausto <jcf@jcfausto.com>
2019-09-25 22:11:10 +0200

    chore: Moved the healthcheck to the so it doesn't get pushed to the bottom everytime a new endpoint is created.

--- api/v1/vehicle_registry.rb
+++ api/v1/vehicle_registry.rb
@@ -5,6 +5,11 @@ module APIv1
   # API responsible for registering and de-registering vehicles
   class VehicleRegistry < Grape::API
     resource :vehicles do
+      desc 'VehicleRegistry API Healthcheck'
+      get :health do
+        { health: 'OK' }
+      end
+
       desc 'Registers a vehicle'
       params do
         requires :id, type: String, desc: 'Vehicle UUID'
@@ -14,11 +19,6 @@ module APIv1
         status :no_content
       rescue StandardError
         status :internal_server_error
-      end
-
-      desc 'VehicleRegistry API Healthcheck'
-      get :health do
-        { health: 'OK' }
       end
     end
   end
