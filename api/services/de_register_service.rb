4a5418afb3535ff2f515afa3f2ade12f396b36dd
Julio Cesar Fausto <jcf@jcfausto.com>
2019-10-12 19:25:34 +0200

    refactor: Remove unnecessary code.

--- api/services/de_register_service.rb
+++ api/services/de_register_service.rb
@@ -4,7 +4,6 @@
 # de-registering vehicles
 class DeRegisterService < BaseService
   def call(vehicle_id)
-    redis_url = ENV['REDIS_URL'] || ENV['REDIS_URL_DEV']
     Redis.new(url: redis_url).del("#{registry_namespace}:#{vehicle_id}")
   end
 end
